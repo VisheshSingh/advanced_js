741460f9c52e8939f90967e256d3d7db6eb45e68
Vishesh Singh <VisheshSingh16@gmail.com>
2024-05-19 11:04:45 -0400

    extract random number with generator function

--- generators.js
+++ generators.js
@@ -17,3 +17,19 @@ console.log(it.next());
 for (let i of generator(items)) {
   console.log(i);
 }
+
+const nums = [90, 80, 40, 50, 10, 20, 60, 30, 70];
+function* extractRandom(arr) {
+  let len = arr.length;
+  let random = Math.floor(Math.random() * len);
+  while (len > 0) {
+    let elem = arr.splice(random, 1)[0];
+    yield elem;
+    len = arr.length;
+  }
+}
+
+const number = extractRandom(nums);
+console.log(number.next());
+console.log(number.next());
+console.log(number.next());
